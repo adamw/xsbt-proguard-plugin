e91e3ca16047d284a60be7a649f92c9d86a35357
adamw <adam@warski.org>
2013-02-19 13:48:19 +0100

    Changing publishing settings

--- project/Build.scala
+++ project/Build.scala
@@ -5,42 +5,6 @@ import Keys._
 import scala.xml.{Elem, Node}
 
 object ProguardPlugin extends Build {
-	def pomPostProcessTask(node: Node) = node match {
-		case xml: Elem =>
-			val children = Seq(
-				<url>https://github.com/adamw/xsbt-proguard-plugin</url>,
-				<licenses>
-					<license>
-						<name>GPLv2</name>
-						<url>http://www.gnu.org/licenses/gpl-2.0.html</url>
-						<distribution>repo</distribution>
-					</license>
-				</licenses>,
-				<scm>
-					<connection>scm:git:git@github.com:adamw/xsbt-proguard-plugin.git</connection>
-					<developerConnection>scm:git:git@github.com:adamw/xsbt-proguard-plugin.git</developerConnection>
-					<url>git@github.com:adamw/xsbt-proguard-plugin.git</url>
-				</scm>,
-				<developers>
-					<developer>
-						<id>siasia</id>
-						<name>Artyom Olshevskiy</name>
-						<email>siasiamail@gmail.com</email>
-					</developer>
-          <developer>
-            <id>adamw</id>
-            <name>Adam Warski</name>
-            <email>adam@warski.org</email>
-          </developer>
-				</developers>,
-				<parent>
-					<groupId>org.sonatype.oss</groupId>
-					<artifactId>oss-parent</artifactId>
-					<version>7</version>
-				</parent>
-			)
-		xml.copy(child = xml.child ++ children)
-	}
 	def rootSettings: Seq[Setting[_]] = Seq(
 		scriptedBufferLog := false,
     scriptedLaunchOpts += "-XX:MaxPermSize=128m",
@@ -53,16 +17,20 @@ object ProguardPlugin extends Build {
 		version <<= sbtVersion(_ + "-0.1.3-SNAPSHOT"),
 		libraryDependencies += "net.sf.proguard" % "proguard-base" % "4.8",
 		scalacOptions += "-deprecation",
-		publishMavenStyle := true,
-		publishTo <<= (version) {
-			version: String =>
-			val ossSonatype = "https://oss.sonatype.org/"
-			if (version.trim.endsWith("SNAPSHOT"))
-				Some("snapshots" at ossSonatype + "content/repositories/snapshots") 
-			else None
-		},
+		publishMavenStyle := false,
+    publishTo <<= (version) { version: String =>
+      val scalasbt = "http://repo.scala-sbt.org/scalasbt/"
+      val (name, url) = if (version.contains("-SNAPSHOT"))
+        ("sbt-plugin-snapshots", scalasbt+"sbt-plugin-snapshots")
+      else
+        ("sbt-plugin-releases", scalasbt+"sbt-plugin-releases")
+      Some(Resolver.url(name, new URL(url))(Resolver.ivyStylePatterns))
+    },
 		credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
-		pomIncludeRepository := ((_) => false),
-		pomPostProcess := (pomPostProcessTask _))
+    homepage := Some(new java.net.URL("https://github.com/adamw/xsbt-proguard-plugin")),
+    licenses := ("GPLv2", new java.net.URL("http://www.gnu.org/licenses/gpl-2.0.html")) :: Nil,
+    scmInfo := Some(ScmInfo(new java.net.URL("https://github.com/adamw/xsbt-proguard-plugin"),
+      "scm:git:git@github.com:adamw/xsbt-proguard-plugin.git"))
+  )
 	lazy val root = Project("root", file(".")) settings(scriptedSettings ++ rootSettings :_*)
 }
